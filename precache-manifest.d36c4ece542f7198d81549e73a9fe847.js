3fdd62f9379289ccfb333421b89b739d784e4547
za-n-6 <zara.nasim96@gmail.com>
2020-03-19 01:05:37 +0500

    Updates

---
+++ precache-manifest.d36c4ece542f7198d81549e73a9fe847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa51e5702c73e8db75e2621647a11422",
+    "url": "/react-work/index.html"
+  },
+  {
+    "revision": "b826be53ed1de34e7007",
+    "url": "/react-work/static/css/main.99758d7b.chunk.css"
+  },
+  {
+    "revision": "9a54a49b636931c70057",
+    "url": "/react-work/static/js/2.8ed49c28.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-work/static/js/2.8ed49c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b826be53ed1de34e7007",
+    "url": "/react-work/static/js/main.3783c7ef.chunk.js"
+  },
+  {
+    "revision": "cea2c7fbaa08f19286d2",
+    "url": "/react-work/static/js/runtime-main.4db3ae45.js"
+  }
+]);
